3524f3e94e919821eca250dc398accebf90802a7
João Fernando <joao_jfmx@outlook.com>
2023-12-03 08:13:53 -0300

    2.0

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -43,7 +43,7 @@ const routes: Routes = [
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes), 
+    RouterModule.forRoot(routes, {useHash: true}), 
     FormsModule,
     ReactiveFormsModule,
     HttpClientModule
